3332c2946527ca04828e6c7634c4903a13b7a252
Eva Wiedmann <evawiedmann@gmail.com>
2020-03-24 11:08:05 -0700

    router structor

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,18 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
+import Header from './Header'
+import MenteeForm from './MenteeForm';
+import MentorForm from './MentorForm';
+import Error404 from './Error404';
+import { Switch, Route } from 'react-router-dom';
 
-class NameForm extends React.Component {
+
+class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {
+      value: ''
+    };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -20,14 +28,33 @@ class NameForm extends React.Component {
   }
 
   render() {
+
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <div>
+      <Header/>
+      <Switch>
+      <Route exact path='/' render={()=><KegList allKegs={this.state.allKegs}/>}/>
+      <Route path='/NewKegForm' render={()=><NewKegForm createKeg={this.handleAddNewKeg} kegList={this.state.allKegs}/>}/>
+
+
+      <Route component={Error404} />
+      </Switch>
+      </div>
     );
   }
-}
+  }
+
+  export default App;
+
+
+//     return (
+//       <form onSubmit={this.handleSubmit}>
+//         <label>
+//           Name:
+//           <input type="text" value={this.state.value} onChange={this.handleChange} />
+//         </label>
+//         <input type="submit" value="Submit" />
+//       </form>
+//     );
+//   }
+// }
