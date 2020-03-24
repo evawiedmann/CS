fce813ba57fd0d65f75866990d9ca66fb87ac948
Eva Wiedmann <evawiedmann@gmail.com>
2020-03-24 14:32:32 -0700

    update

--- src/components/MentorForm.jsx
+++ src/components/MentorForm.jsx
@@ -3,25 +3,37 @@ import { Router } from 'react-router-dom';
 
 function MentorForm(props){
   let _name = null
-  let _brand = null
-  let _price = 0
-  let _abv = 0
+  let _position = null
+  let _company = null
+  let _gender = null
+  let _genderPref = null
+  let _yearsInTech = null
+  let _specialty = null
+  let _priorities = null
 
   function mentor(event){
     event.preventDefault();
     props.createMentor({
       name: _name.value,
-      brand: _brand.value,
-      price: _price.value,
-      abv: _abv.value,
-      remainingPints: [124]
+      position: _position.value,
+      company: _company.value,
+      gender: _gender.value,
+      genderPref: _genderPref.value,
+      yearsInTech: _yearsInTech.value,
+      specialty: _specialty.value,
+      priorities: _priorities.value,
     });
 
     _name.value = '';
-    _brand.value = '';
-    _price.value = '';
-    _abv.value = '';
-  }
+    _position.value = '';
+    _company.value = '';
+    _gender.value = '';
+    _genderPref.value = '';
+    _yearsInTech.value = '';
+    _specialty.value = '';
+    _priorities.value = '';
+    }
+
   let post = {
 
     height: '70px',
@@ -55,14 +67,49 @@ function MentorForm(props){
   }
 
   return (
-    <div onSubmit={mentor}>
+    <div onSubmit={mentee}>
     <div className='form'>
     <form>
     <input
     type='text'
     id='name'
-    placeholder='Beer Name'
+    placeholder='First and Last Name'
     ref={(input) => {_name = input}}/>
+    <input
+    type='text'
+    id='position'
+    placeholder='Position Title'
+    ref={(input) => {_position = input}}/>
+    <input
+    type='text'
+    id='company'
+    placeholder='Company or School Name'
+    ref={(input) => {_company = input}}/>
+    <input
+    type='text'
+    id='gender'
+    placeholder='Gender'
+    ref={(input) => {_gender = input}}/>
+    <input
+    type='text'
+    id='genderPref'
+    placeholder='Mentor Gender Preference'
+    ref={(input) => {_genderPref = input}}/>
+    <input
+    type='text'
+    id='yearsInTech'
+    placeholder='Years in Tech'
+    ref={(input) => {_yearsInTech = input}}/>
+    <input
+    type='text'
+    id='specialty'
+    placeholder='Specialty'
+    ref={(input) => {_specialty = input}}/>
+    <input
+    type='text'
+    id='priorities'
+    placeholder='Priorities'
+    ref={(input) => {_priorities = input}}/>
     <div className='button'>
     <input type="submit" value="Add" />
     </div>
